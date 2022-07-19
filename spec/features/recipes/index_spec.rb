1cc6a958e91956ed779b655dfdd12ff52d4da5dc
Lucas Colwell <colwellc37@gmail.com>
2022-07-19 16:42:26 -0600

    Write test for story 1

--- spec/features/recipes/index_spec.rb
+++ spec/features/recipes/index_spec.rb
@@ -12,9 +12,9 @@ require 'rails_helper'
 
 RSpec.describe 'recipes index page' do
   it 'shows the name of all recipes and info (Name, Complexity, Genre)' do
-    spaghetti = Recipe.create!(Name: "Spaghetti", Complexity: 2, Genre: "Italian")
-    chili = Recipe.create!(Name: "Chili", Complexity: 3, Genre: "Texan")
-    hummus = Recipe.create!(Name: "Hummus", Complexity: 1, Genre: "Med")
+    spaghetti = Recipe.create!(name: "Spaghetti", complexity: 2, genre: "Italian")
+    chili = Recipe.create!(name: "Chili", complexity: 3, genre: "Texan")
+    hummus = Recipe.create!(name: "Hummus", complexity: 1, genre: "Med")
 
     visit "/recipes"
     # within "#courses-#{spaghetti.id}" do
